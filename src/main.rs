494dd1c2432fc394e15d3a846079eb1feb6da151
Artem Fokin <artem@favrit.com>
2022-09-12 15:08:23 +0200

    Parse json and populate result table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -16,8 +17,22 @@ fn main() -> Result<()> {
         .with_context(|| format!("Failed to read {}", &args.path))?;
     let buffered = BufReader::new(file);
 
+    let mut result_table = HashMap::<String, (usize, usize)>::new();
+
     for line in buffered.lines() {
-        println!("{}", line?);
+        let line_str = line.as_ref().unwrap();
+
+        let parsed = json::parse(line_str)
+            .with_context(|| format!("Failed to parse line [{}]", line_str))?;
+        let typ = parsed["type"].as_str().unwrap(); // TODO handle unwrap
+        let size = std::mem::size_of_val(&line_str); // TODO wrong size here
+
+        if result_table.contains_key(&typ.to_string()) {
+            let (count, curr_size) = result_table[&typ.to_string()];
+            result_table.insert(typ.to_string(), (count + 1, curr_size + size));
+        } else {
+            result_table.insert(typ.to_string(), (1, size));
+        }
     }
 
     Ok(())
