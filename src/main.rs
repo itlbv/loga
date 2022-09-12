00fc976a1c4b949d8bc6fcc747cf0c676fa50618
Artem Fokin <artem@favrit.com>
2022-09-12 18:35:54 +0200

    Add error handling and failed lines printing

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,31 @@ fn main() -> Result<()> {
     let buffered = BufReader::new(file);
 
     let mut result_table = HashMap::<String, (usize, usize)>::new();
+    let mut failed_lines = vec![];
 
     for line in buffered.lines() {
-        let line_str = line.as_ref().unwrap();
+        let line_str = line.as_ref()
+            .expect("Unexpected error processing the input file.");
+
+        if line_str.is_empty() {
+            continue;
+        }
 
         let parsed = json::parse(line_str)
             .with_context(|| format!("Failed to parse line [{}]", line_str))?;
-        let typ = parsed["type"].as_str().unwrap(); // TODO handle unwrap
-        let size = std::mem::size_of_val(&line_str); // TODO wrong size here
 
-        if result_table.contains_key(&typ.to_string()) {
-            let (count, curr_size) = result_table[&typ.to_string()];
-            result_table.insert(typ.to_string(), (count + 1, curr_size + size));
-        } else {
-            result_table.insert(typ.to_string(), (1, size));
+        match parsed["type"].as_str() {
+            None => failed_lines.push(String::from(line_str)),
+            Some(typ) => {
+                let size = line_str.capacity(); // TODO wrong size here
+
+                if result_table.contains_key(&typ.to_string()) {
+                    let (count, curr_size) = result_table[&typ.to_string()];
+                    result_table.insert(typ.to_string(), (count + 1, curr_size + size));
+                } else {
+                    result_table.insert(typ.to_string(), (1, size));
+                }
+            }
         }
     }
 
@@ -45,6 +56,16 @@ fn main() -> Result<()> {
     print_table_row(longest_type, &"type name".to_string(), &"count".to_string(), &"size".to_string());
     for (typ, (count, size)) in &result_table {
         print_table_row(longest_type, typ, &count.to_string(), &size.to_string());
+    }
+
+    if failed_lines.is_empty() {
+        println!("All lines successfully processed.")
+    } else {
+        println!("Field [type] was not found in {} lines.", failed_lines.len());
+        println!("Failed lines:");
+        for failed_line in failed_lines {
+            println!("{}", failed_line);
+        }
     }
 
     Ok(())
